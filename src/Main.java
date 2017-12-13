9c04a47911077abdb4b8e3379a34caf6fcc3cac4
Verti <Verti@DESKTOP-8NCCP6J>
2017-12-13 13:20:58 -0500

    Update 1.0

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 public static void main(String[] args)
 {
+	//Creating condition and then creating an object to run the class.
 	String conditionOne = "";
 	DatabaseQuery dbQuery = new DatabaseQuery(conditionOne);
 	dbQuery.run();
